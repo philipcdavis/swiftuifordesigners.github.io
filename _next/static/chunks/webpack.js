6f06fde9efd42da0b3bf23a9fabeedb4a9a0e882
Philip Davis <philipcdavis@gmail.com>
2021-01-02 18:57:27 -0500

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "870cf98bbf014cb36e40";
+/******/ 	var hotCurrentHash = "fce254803dbbe16a7bb2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
