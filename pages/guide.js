bf1c8513f6522d27fac04991aaba0f54360377bd
Philip Davis <philipcdavis11@gmail.com>
2021-01-02 18:04:14 -0500

    Fixed some spacing

--- pages/guide.js
+++ pages/guide.js
@@ -103,7 +103,7 @@ export default function Guide() {
           In order to use SwiftUI with live preview you'll need a Mac (or iPad),
           and Xcode.
         </p>
-        <Flex mx={-2} mb={"50px"} flexWrap="wrap">
+        <Flex mx={-2} mb={"90px"} flexWrap="wrap">
           <SystemCard
             label="MacOS"
             version="10.15+"
